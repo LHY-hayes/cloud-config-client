7f99791eb5871e438b582981df0691cdc55ca5a2
775788531@qq.com <LINhuaiyou.4013>
2020-03-05 19:21:26 +0800

    update

--- src/main/java/com/hayes/code/cloudconfigclient/controller/TestController.java
+++ src/main/java/com/hayes/code/cloudconfigclient/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.hayes.code.cloudconfigclient.controller;
 
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
